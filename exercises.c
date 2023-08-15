1ac83016efab234d4eb30f74617006fe2da4cb5f
Replit user <>
2023-08-15 22:04:34 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -44,7 +44,17 @@ Descripción: Escribe una función que tome un arreglo de enteros
 y su tamaño, y luego devuelva un nuevo arreglo que contenga solo
 los números pares del arreglo original.
 */
-int *filterEvenNumbers(int arr[], int size, int *newSize) { return NULL; }
+int *filterEvenNumbers(int arr[], int size, int *newSize) { 
+  
+  /*for (int i=0;i<size;i++){
+    if ()
+  }*/
+
+  
+  
+  
+  return *newSize; 
+}
 
 /*
 Ejercicio 4: Fusión de dos Arreglos Ordenados
