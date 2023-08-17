0a6c1692daad2c187b97fa6451e637075cfe993d
Replit user <>
2023-08-17 23:17:14 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -108,7 +108,7 @@ y luego devuelva 1 si el arreglo está ordenado en orden ascendente,
 */
 int checkSorted(int arr[], int size) { 
   int contAsc=0, contDes=0;
-  for (int i=0;i<size;i++){
+  for (int i=0;i<size-1;i++){
     if (arr[i]<arr[i+1]){
       contAsc++;
     }
@@ -126,7 +126,8 @@ int checkSorted(int arr[], int size) {
     
   }
   
-  return 0; }
+  return 0; 
+}
 
 /*
 Ejercicio 6: Información de una Biblioteca
