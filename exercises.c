bcf279ac572919da16593b06f51424913f059940
Replit user <>
2023-08-17 20:43:40 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -80,11 +80,12 @@ void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,int result[]
     for(int j=0;j<size1;j++){
       result[i]=arr1[j];
     }
+    
     for(int z=0;z<size2;z++){
       result[i]=arr2[z];
     }
   }
-  /*
+  
   for (int i=0;i < size3-1;i++){
     for(int j=0;j<size3-i-1;j++){
       if (result[j] > result[j+1]){
@@ -93,7 +94,7 @@ void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,int result[]
         result[j+1]=temp;
       }
     }
-  }*/
+  }
 
   
 }
